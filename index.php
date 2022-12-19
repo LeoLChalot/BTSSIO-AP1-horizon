dbac1170eaec80e8f6602ee5b1c9db3ba431ad36
LeoLChalot <larouchalotleo@gmail.com>
2022-12-19 20:57:55 +0100

    Small device home OK

--- index.php
+++ index.php
@@ -10,12 +10,29 @@ require_once(__DIR__ . '/includes/header.php');
         </h1>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui, ipsum debitis provident voluptatem aperiam quidem perferendis suscipit eius deleniti recusandae quibusdam, et natus unde consectetur?</p>
     </header>
-    <section id="newsletter">
+    <section id="index-newsletter">
         <p>Restez informés avec notre Newsletter !</p>
         <form action="#">
             <input type="mail" placeholder="Entrez votre mail...">
-            <input type="submit" value="Rester informé">
+            <input type="submit" value="Je reste informé !">
         </form>
+    </section>
+    <section id="index-worklist">
+        <div class="work-card">
+            <img class="img-worklist" src="assets/images/logo2.png" alt="img prestation">
+            <h3>Web Design</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis deserunt esse ipsum officia quas maiores.</p>
+        </div>
+        <div class="work-card">
+            <img class="img-worklist" src="assets/images/logo2.png" alt="img prestation">
+            <h3>Website Maintenance</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium voluptates laboriosam adipisci possimus officia cumque.</p>
+        </div>
+        <div class="work-card">
+            <img class="img-worklist" src="assets/images/logo2.png" alt="img prestation">
+            <h3>Website Hosting</h3>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo officiis saepe ipsum harum eaque. Iste.</p>
+        </div>
     </section>
 
 </main>
