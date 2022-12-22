afd09cb8de6a9990cd4f19da3d29b36c2fa9aa50
LeoLChalot <larouchalotleo@gmail.com>
2022-12-22 18:21:01 +0100

    Dépôt CGU

--- includes/footer.php
+++ includes/footer.php
@@ -1,8 +1,12 @@
 </div> <!-- FIN #page-content (header.php)-->
 <footer id="footer">
     <p>Horizon Dev Web, Copyright&copy;, 2022</p>
+    <p>
+        <a href="#">Contact</a>
+        <span class="highlight">|</span>
+        <a href="CGU.php" target="_blank" title="Lire les CGU">CGU</a>
+    </p>
 </footer>
-<!-- <script src="" async defer></script> -->
 </body>
 
 </html>
